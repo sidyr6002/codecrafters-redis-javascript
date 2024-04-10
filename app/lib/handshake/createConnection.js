af474d7fce326cde50b31b4b2b1c799f4370702e
sidyr6002 <siddhardh6002@gmail.com>
2024-04-10 22:55:41 +0530

    added step 3 of handshake

--- app/lib/handshake/createConnection.js
+++ app/lib/handshake/createConnection.js
@@ -1,29 +1,52 @@
 const net = require("net");
 
-const conf = [
-    "*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n",
-    "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n",
-];
+const commands = {
+    PING: "*1\r\n$4\r\nping\r\n",
+    REPLCONF_PORT: "*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n",
+    REPLCONF_PSYNC: "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n",
+    PSYNC: "*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n"
+};
+
+function sendCommand(socket, command) {
+    socket.write(commands[command]);
+}
 
 const createConnection = (host, port) => {
     const socket = new net.Socket();
+
     socket.connect(port, host);
 
     socket.on("connect", () => {
-        socket.write("*1\r\n$4\r\nping\r\n");
+        sendCommand(socket, "PING");
     });
 
-    //add commands
-    socket.on("data", () => {
-        conf.forEach((c) => {
-            socket.write(c);
-        })
-    })
+    socket.on("data", (data) => {
+        const buffer = data.toString().trim();
+        console.log("Data: ", buffer);
+
+        switch (buffer) {
+            case "+PONG":
+                sendCommand(socket, "REPLCONF_PORT");
+                break;
+            case "+OK":
+                if (!socket.sentReplconfCapa) {
+                    socket.sentReplconfCapa = true;
+                    sendCommand(socket, "REPLCONF_PSYNC");
+                } else {
+                    sendCommand(socket, "PSYNC");
+                }
+                break;
+            default:
+                console.log("Unhandled response:", buffer);
+        }
+    });
 
     socket.on("error", (err) => {
         console.log("Client error: ", err);
-    })
-};
+        socket.destroy(); // Close the socket on error
+    });
 
+    return socket;
+};
 
 module.exports = { createConnection };
